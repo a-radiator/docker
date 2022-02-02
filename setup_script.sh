0b8f7fc83970e34d5877223289e67fb16c81f786
a-radiator <98500022+a-radiator@users.noreply.github.com>
2022-02-02 01:04:30 +0000

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -9,11 +9,7 @@ apt-get install ca-certificates curl gnupg lsb-release software-properties-commo
 #install gpg key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - 2> /dev/null
 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu/dists $(lsb_release -cs) stable"
 
 #install docker community edition
 apt-get install docker-ce docker-ce-cli containerd.io -y

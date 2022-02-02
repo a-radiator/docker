386d5a91b23831f83caad5403ad442b3fa07cf0c
a-radiator <98500022+a-radiator@users.noreply.github.com>
2022-02-02 01:28:24 +0000

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#apt-get update -y
-
-cat /etc/os-release
 
 #install required packages for docker engine
 apt-get install ca-certificates curl gnupg lsb-release software-properties-common -y  
 
 #install gpg key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - 2> /dev/null
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
+#add docker ce repo
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu/dists $(lsb_release -cs) stable"
 
 #install docker community edition
@@ -16,6 +14,9 @@ apt-get install docker-ce docker-ce-cli containerd.io -y
 
 #build image 
 docker build -t hello-peak . 
+apt-get update -y 
 
 #run container
 docker run hello-peak
+
+

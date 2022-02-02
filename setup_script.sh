25f88b1c60ae3206524d468e4647387d117a6447
a-radiator <98500022+a-radiator@users.noreply.github.com>
2022-02-02 01:01:03 +0000

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -11,6 +11,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - 2> /dev/
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 #install docker community edition
 apt-get install docker-ce docker-ce-cli containerd.io -y
 

a26ea3aa68a54bbacd0214eca0abad25cd5d179a
a-radiator <98500022+a-radiator@users.noreply.github.com>
2022-02-02 03:10:03 +0000

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-apt update -y 
+apt update -y  && 
 
 #install required packages for docker engine
-apt install ca-certificates curl gnupg lsb-release software-properties-common apt-transport-https -y
+apt install ca-certificates curl gnupg lsb-release software-properties-common apt-transport-https -y &&
 
 #repo install
-echo "deb https://download.docker.com/linux/debian stretch stable" > /etc/apt/sources.list.d/docker.list
+echo "deb https://download.docker.com/linux/debian stretch stable" > /etc/apt/sources.list.d/docker.list &&
 
 #key
-wget --quiet --output-document - https://download.docker.com/linux/debian/gpg  | sudo apt-key add - > /dev/null
+wget --quiet --output-document - https://download.docker.com/linux/debian/gpg  | sudo apt-key add - > /dev/null && 
 
 #clear
-apt-cache policy docker-ce docker-ce-cli
+apt-cache policy docker-ce docker-ce-cli && 
 
 #install docker community edition
-apt install docker-ce docker-ce-cli containerd.io -y
-apt update -y
+apt install docker-ce docker-ce-cli containerd.io -y &&
+apt update -y &&
 
 #build image
-docker build -t hello-peak .
+docker build -t hello-peak . &&
 
 #run container
 docker run hello-peak
